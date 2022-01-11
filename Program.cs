6d712edc82952414a574688d45096b4f0ae70d42
Wesley Ferreira <wesleyferreirasb@gmail.com>
2022-01-11 03:16:25 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Exercicios;
+
+namespace algoritmos_teoria_e_pratica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var desordenada = new int[] { 5, 2, 4, 6, 1, 3 };
+            foreach (var item in desordenada)
+            {
+                System.Console.Write($"{item},");
+            }
+            System.Console.WriteLine("");
+            Ordenacoes.Ordenacao(desordenada);
+            foreach (var item in desordenada)
+            {
+                System.Console.Write($"{item},");
+            }
+        }
+    }
+}
